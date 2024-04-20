6101fb8f21b09658313b5de45836b2451775ebbd
kimseryeong <kxxoo322@gmail.com>
2024-04-20 16:42:35 +0900

    자바스크립트 defer/async

---
+++ DreamCoding/main.js
@@ -0,0 +1,8 @@
+//strict 모드 
+// Javascript 는 매우 유연한 언어
+// 유연한 언어 === 위험(많은 실수가 있을 수 있음)
+// 자바스크립트 엔진이 효율적으로 빠르게 자바스크립트 분석 가능 (성능 개선 효과)
+// ECMAScript 5에 추가되어 있음
+'use strict';
+
+console.log('Hello World');
