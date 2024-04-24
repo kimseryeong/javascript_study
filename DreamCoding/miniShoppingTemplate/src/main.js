5a7c4b98ccdcbed698d364422f18ed535b0be79a
kimseryeong <kxxoo322@gmail.com>
2024-04-24 17:56:11 +0900

    project

---
+++ DreamCoding/miniShoppingTemplate/src/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const logoBtn = document.getElementById('logo');
+
+// 로고 클릭 함수
+function fnLogoBtn(){
+    console.log('로고 클릭됨');
+
+    //전체 목록 출력
+}
+
+/**
+ * 로직 생각해보기
+ * 
+ * 티셔츠 클릭 -> 서버에서 티셔츠에 해당하는 데이터 받아오기
+ */
+
+function getAllClothes(item){
+    //데이터 받아오기
+
+}
