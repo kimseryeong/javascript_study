c68e9f46284da5fc64a0278e3d41dff49576b535
kimseryeong <kxxoo322@gmail.com>
2024-04-29 23:48:55 +0900

    고양이 api 가져오기

--- API/main.js
+++ API/main.js
@@ -11,5 +11,25 @@ let requestOptions = {
 
 fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => loadCatInfo(result[0]))
     .catch(error => console.log('error', error));
+
+const image = document.querySelector('.image');
+const desc = document.querySelector('.description');
+
+//API) 고양이 이미지, 이름, 설명 로드하는 함수
+function loadCatInfo(result){
+    console.log(result);
+    console.log(result.breeds);
+    const breeds = result.breeds[0];
+
+    image.innerHTML = `<img src="${result.url}"/>`;
+    desc.innerHTML = `
+        <div class="title">    
+            <h2>[ ${breeds.name} ]</h2>
+            <span class="left">${breeds.origin}</span>
+        </div>
+        <b>Description</b>
+        <p>${breeds.description.replace('\n', '<br/>')}</p>
+    `;
+}
