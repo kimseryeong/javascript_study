bc4a7e22c04477f7884df10fbcbf871473d45d88
kimseryeong <kxxoo322@gmail.com>
2024-05-02 14:05:04 +0900

    고양이 API 가져오기

--- API/main.js
+++ API/main.js
@@ -11,11 +11,15 @@ let requestOptions = {
 
 fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
     .then(response => response.json())
-    .then(result => loadCatInfo(result[0]))
+    .then(result => { 
+        loadCatInfo(result[0]);
+        ChangeCat();
+    })
     .catch(error => console.log('error', error));
 
 const image = document.querySelector('.image');
-const desc = document.querySelector('.description');
+const intro = document.querySelector('.intro');
+const desc = document.querySelector('.desc');
 
 //API) 고양이 이미지, 이름, 설명 로드하는 함수
 function loadCatInfo(result){
@@ -24,12 +28,16 @@ function loadCatInfo(result){
     const breeds = result.breeds[0];
 
     image.innerHTML = `<img src="${result.url}"/>`;
-    desc.innerHTML = `
-        <div class="title">    
-            <h2>[ ${breeds.name} ]</h2>
-            <span class="left">${breeds.origin}</span>
-        </div>
-        <b>Description</b>
-        <p>${breeds.description.replace('\n', '<br/>')}</p>
+    intro.innerHTML = `
+        <h2>[ ${breeds.name} ]</h2>
+        <span class="left">from. ${breeds.origin}</span>
     `;
+    desc.innerHTML = `${breeds.description.replace("\n\t", '<br/>')}`;
+}
+
+function ChangeCat(result){
+    document.querySelector('.changeCatBtn').addEventListener('click', (e) => {
+        location.href = location.href;
+        // loadCatInfo(result);
+    })
 }
